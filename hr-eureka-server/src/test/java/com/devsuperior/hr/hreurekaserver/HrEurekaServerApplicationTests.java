29644c495ce64d4682c52d95c0cf1642098adbbf
Luiz Henrique Kafer Silva <luizhenrique-rs@windowslive.com>
2022-01-10 05:43:05 -0300

    Project hr-eureka-server created

---
+++ hr-eureka-server/src/test/java/com/devsuperior/hr/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hr.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
