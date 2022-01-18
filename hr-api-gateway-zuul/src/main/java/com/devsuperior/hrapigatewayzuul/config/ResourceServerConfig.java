a1a9b03d1932af6449d630455fe1ff54e63b37aa
Luiz Henrique Kafer Silva <luizhenrique-rs@windowslive.com>
2022-01-18 08:27:48 -0300

    CORs configuration

--- hr-api-gateway-zuul/src/main/java/com/devsuperior/hrapigatewayzuul/config/ResourceServerConfig.java
+++ hr-api-gateway-zuul/src/main/java/com/devsuperior/hrapigatewayzuul/config/ResourceServerConfig.java
@@ -1,9 +1,15 @@
 package com.devsuperior.hrapigatewayzuul.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
@@ -15,7 +21,7 @@ public class ResourceServerConfig {
     private final AuthenticationManager authenticationManager;
     private final BearerTokenConverter bearerTokenConverter;
 
-    private static final String[] PUBLIC = { "/hr-oauth/oauth/token" };
+    private static final String[] PUBLIC = { "/hr-oauth/oauth/token", "/hr-worker/" };
     private static final String[] OPERATOR = { "/hr-worker/**" };
     private static final String[] ADMIN = { "/hr-payroll/**", "/hr-user/**", "/hr-oauth/**", "/actuactor/**",
             "/hr-worker/actuator/**", "/hr-oauth/actuator/**" };
@@ -32,6 +38,18 @@ public class ResourceServerConfig {
                 .authenticationManagerResolver(exchange -> Mono.just(authenticationManager))
                 .bearerTokenConverter(bearerTokenConverter);
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfiguration() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Arrays.asList("*"));
+        corsConfig.setAllowedHeaders(Arrays.asList("*"));
+        corsConfig.setAllowedMethods(Arrays.asList("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+        return source;
     }
 
 }
