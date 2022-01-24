91bfcab3b3114ec8fe1f8a9fc9e96194bbde2db1
Luiz Henrique Kafer Silva <luizhenrique-rs@windowslive.com>
2022-01-24 16:59:08 -0300

    Update WorkerResource.java

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -12,6 +12,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,9 +33,9 @@ public class WorkerResource {
     private final WorkerRepository repository;
 
     @GetMapping("/configs")
-    public ResponseEntity<Void> getConfigs() {
+    public ResponseEntity<String> getConfigs(@RequestHeader("Authorization") String authorization) {
         log.info("CONFIG = " + testConfig);
-        return ResponseEntity.noContent().build();
+        return ResponseEntity.ok(authorization);
     }
 
     @GetMapping
