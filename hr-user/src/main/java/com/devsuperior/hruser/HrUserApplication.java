9b565a5f376aa155771a3a21b6571cfe73174fde
Luiz Henrique Kafer Silva <luizhenrique-rs@windowslive.com>
2022-01-24 12:22:40 -0300

    hr-user container

--- hr-user/src/main/java/com/devsuperior/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/devsuperior/hruser/HrUserApplication.java
@@ -1,16 +1,12 @@
 package com.devsuperior.hruser;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-import lombok.extern.log4j.Log4j2;
-
 @EnableEurekaClient
 @SpringBootApplication
-@Log4j2
 public class HrUserApplication implements CommandLineRunner {
 
     // @Autowired
