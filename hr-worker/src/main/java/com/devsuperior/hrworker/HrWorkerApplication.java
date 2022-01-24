f0599c87434cb510495f6df6348b19ea0ec5a41a
Luiz Henrique Kafer Silva <luizhenrique-rs@windowslive.com>
2022-01-24 09:05:25 -0300

    hr-worker Postgres dev profile

--- hr-worker/src/main/java/com/devsuperior/hrworker/HrWorkerApplication.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/HrWorkerApplication.java
@@ -1,15 +1,28 @@
 package com.devsuperior.hrworker;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 @EnableEurekaClient
 @SpringBootApplication
-public class HrWorkerApplication {
+public class HrWorkerApplication implements CommandLineRunner {
+
+    @Value("${test.config}")
+    private String testConfig;
 
     public static void main(String[] args) {
         SpringApplication.run(HrWorkerApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("test.config: " + testConfig);
     }
 
 }
